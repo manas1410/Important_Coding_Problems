fe5045d71b168842be7b7a5e04d2219e5203a9e9
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2022-01-14 00:42:18 +0530

    Update reverse_list.py

--- Arrays/reverse_list.py
+++ Arrays/reverse_list.py
@@ -5,6 +5,11 @@ def reverse_list(lis,start,end):
         lis[start],lis[end] = lis[end],lis[start]
         start,end = start+1, end-1
     print(lis)
+    
+#or
+
+def reverse_list1(lis,start,end):
+    print(lis[::-1])
         
 input_list = input().split()
 reverse_list(input_list,0,len(input_list)-1)
